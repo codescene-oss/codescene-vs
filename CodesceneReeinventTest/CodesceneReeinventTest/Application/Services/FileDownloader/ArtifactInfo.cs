3c2da5d32c99e1f0ce669e8feab8573a5619d856
aminaovcina-ree <amina.ovcina@reeinvent.com>
2024-10-02 13:26:42 +0200

    Code Refactor.

--- CodesceneReeinventTest/CodesceneReeinventTest/Application/Services/FileDownloader/ArtifactInfo.cs
+++ CodesceneReeinventTest/CodesceneReeinventTest/Application/Services/FileDownloader/ArtifactInfo.cs
@@ -6,33 +6,14 @@ namespace CodesceneReeinventTest.Application.Services.FileDownloader
     {
         public readonly string ExtensionPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
-        private const string RequiredDevToolsVersion = "7788d46b2a8c65062d79f790444e52b0dd12cfb8";
-        private const string Platform = "win32";
-        private const string Arch = "x64";
+        private const string REQUIRED_DEV_TOOLS_VERSION = "7788d46b2a8c65062d79f790444e52b0dd12cfb8";
+        private const string PLATFORM = "win32";
+        private const string ARCH = "x64";
 
-        private string BinaryName => $"cs-{Platform}-{Arch}.exe";
-        public string ArtifactName => $"codescene-cli-ide-windows-amd64-{RequiredDevToolsVersion}.zip";
-
-        public string AbsoluteDownloadPath
-        {
-            get
-            {
-                return Path.Combine(ExtensionPath, ArtifactName);
-            }
-        }
-        public string AbsoluteBinaryPath
-        {
-            get
-            {
-                return Path.Combine(ExtensionPath, BinaryName);
-            }
-        }
-        public string ExecFromZipPath
-        {
-            get
-            {
-                return Path.Combine(ExtensionPath, "cs.exe");
-            }
-        }
+        private string BinaryName => $"cs-{PLATFORM}-{ARCH}.exe";
+        public string ArtifactName => $"codescene-cli-ide-windows-amd64-{REQUIRED_DEV_TOOLS_VERSION}.zip";
+        public string AbsoluteDownloadPath => Path.Combine(ExtensionPath, ArtifactName);
+        public string AbsoluteBinaryPath => Path.Combine(ExtensionPath, BinaryName);
+        public string ExecFromZipPath => Path.Combine(ExtensionPath, "cs.exe");
     }
 }
