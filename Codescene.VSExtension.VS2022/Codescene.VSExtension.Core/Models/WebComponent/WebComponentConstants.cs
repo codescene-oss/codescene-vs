e26fcf99a82af4f923e99991419c80401c2424e9
Selma Copra <selma.copra@codescene.com>
2025-06-09 13:44:54 +0200

    removed unused doc types

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Models/WebComponent/WebComponentConstants.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Models/WebComponent/WebComponentConstants.cs
@@ -27,45 +27,5 @@
             public const string REQUEST_AND_PRESENT_REFACTORING = "request-and-present-refactoring";
             public const string UPDATE_RENDERER = "update-renderer";
         }
-
-        public class DocTypes
-        {
-            public const string DOCS_CODE_HEALTH_MONITOR = "docs_code_health_monitor";
-            public const string DOCS_GENERAL_CODE_HEALTH = "docs_general_code_health";
-            public const string DOCS_IMPROVEMENT_GUIDES_BUMPY_ROAD_AHEAD = "docs_improvement_guides_bumpy_road_ahead";
-            public const string DOCS_IMPROVEMENT_GUIDES_COMPLEX_CONDITIONAL = "docs_improvement_guides_complex_conditional";
-            public const string DOCS_IMPROVEMENT_GUIDES_COMPLEX_METHOD = "docs_improvement_guides_complex_method";
-            public const string DOCS_IMPROVEMENT_GUIDES_DEEP_NESTED_COMPLEXITY = "docs_improvement_guides_deep_nested_complexity";
-            public const string DOCS_IMPROVEMENT_GUIDES_LARGE_METHOD = "docs_improvement_guides_large_method";
-            public const string DOCS_IMPROVEMENT_GUIDES_MODULARITY_IMPROVEMENT = "docs_improvement_guides_modularity_improvement";
-            public const string DOCS_ISSUES_BRAIN_CLASS = "docs_issues_brain_class";
-            public const string DOCS_ISSUES_BRAIN_METHOD = "docs_issues_brain_method";
-            public const string DOCS_ISSUES_BUMPY_ROAD_AHEAD = "docs_issues_bumpy_road_ahead";
-            public const string DOCS_ISSUES_CODE_DUPLICATION = "docs_issues_code_duplication";
-            public const string DOCS_ISSUES_COMPLEX_CONDITIONAL = "docs_issues_complex_conditional";
-            public const string DOCS_ISSUES_COMPLEX_METHOD = "docs_issues_complex_method";
-            public const string DOCS_ISSUES_CONSTRUCTOR_OVER_INJECTION = "docs_issues_constructor_over_injection";
-            public const string DOCS_ISSUES_DEEP_GLOBAL_NESTED_COMPLEXITY = "docs_issues_deep_global_nested_complexity";
-            public const string DOCS_ISSUES_DEEP_NESTED_COMPLEXITY = "docs_issues_deep_nested_complexity";
-            public const string DOCS_ISSUES_DUPLICATED_ASSERTION_BLOCKS = "docs_issues_duplicated_assertion_blocks";
-            public const string DOCS_ISSUES_DUPLICATED_FUNCTION_BLOCKS = "docs_issues_duplicated_function_blocks";
-            public const string DOCS_ISSUES_EXCESS_NUMBER_OF_FUNCTION_ARGUMENTS = "docs_issues_excess_number_of_function_arguments";
-            public const string DOCS_ISSUES_FILE_SIZE_ISSUE = "docs_issues_file_size_issue";
-            public const string DOCS_ISSUES_GLOBAL_CONDITIONALS = "docs_issues_global_conditionals";
-            public const string DOCS_ISSUES_HIGH_DEGREE_OF_CODE_DUPLICATION = "docs_issues_high_degree_of_code_duplication";
-            public const string DOCS_ISSUES_LARGE_ASSERTION_BLOCKS = "docs_issues_large_assertion_blocks";
-            public const string DOCS_ISSUES_LARGE_EMBEDDED_CODE_BLOCK = "docs_issues_large_embedded_code_block";
-            public const string DOCS_ISSUES_LARGE_METHOD = "docs_issues_large_method";
-            public const string DOCS_ISSUES_LINES_OF_CODE_IN_A_SINGLE_FILE = "docs_issues_lines_of_code_in_a_single_file";
-            public const string DOCS_ISSUES_LINES_OF_DECLARATIONS_IN_A_SINGLE_FILE = "docs_issues_lines_of_declarations_in_a_single_file";
-            public const string DOCS_ISSUES_LOW_COHESION = "docs_issues_low_cohesion";
-            public const string DOCS_ISSUES_MISSING_ARGUMENTS_ABSTRACTIONS = "docs_issues_missing_arguments_abstractions";
-            public const string DOCS_ISSUES_MODULARITY_ISSUE = "docs_issues_modularity_issue";
-            public const string DOCS_ISSUES_NUMBER_OF_FUNCTIONS_IN_A_SINGLE_MODULE = "docs_issues_number_of_functions_in_a_single_module";
-            public const string DOCS_ISSUES_OVERALL_CODE_COMPLEXITY = "docs_issues_overall_code_complexity";
-            public const string DOCS_ISSUES_POTENTIALLY_LOW_COHESION = "docs_issues_potentially_low_cohesion";
-            public const string DOCS_ISSUES_PRIMITIVE_OBSESSION = "docs_issues_primitive_obsession";
-            public const string DOCS_ISSUES_STRING_HEAVY_FUNCTION_ARGUMENTS = "docs_issues_string_heavy_function_arguments";
-        }
     }
 }
