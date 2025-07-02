13a3d48f23c58c15248848a4a0a5b17cd2c634cf
Selma Copra <selma.copra@codescene.com>
2025-07-02 10:24:00 +0200

    add telemetry event constants and extension metadata provider

---
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.VS2022/Util/ExtensionMetadataProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Codescene.VSExtension.VS2022.Util;
+
+public static class ExtensionMetadataProvider
+{
+    public static string GetVersion() => Vsix.Version;
+    public static string GetDisplayName() => Vsix.Name;
+    public static string GetDescription() => Vsix.Description;
+    public static string GetPublisher() => Vsix.Author;
+}
+
