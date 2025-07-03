96ca98af448ec40f6c2ad831bc5b48675c3d53f1
Selma Copra <selma.copra@codescene.com>
2025-07-03 14:39:11 +0200

    send opened settings event

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.VS2022/Commands/OpenSettingsCommand.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.VS2022/Commands/OpenSettingsCommand.cs
@@ -1,4 +1,6 @@
-﻿using Community.VisualStudio.Toolkit;
+﻿using Codescene.VSExtension.Core.Application.Services.Telemetry;
+using Codescene.VSExtension.Core.Application.Services.Util;
+using Community.VisualStudio.Toolkit;
 using Microsoft.VisualStudio.Shell;
 using System.Threading.Tasks;
 namespace Codescene.VSExtension.VS2022.Commands;
@@ -6,15 +8,20 @@ namespace Codescene.VSExtension.VS2022.Commands;
 [Command(PackageGuids.CodeSceneCmdSetString, PackageIds.OpenSettings)]
 internal sealed class OpenSettingsCommand : BaseCommand<OpenSettingsCommand>
 {
-    //protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
-    //{
-    //    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-    //    VS2022Package.Instance.ShowOptionPage(typeof(OptionsProvider.GeneralOptions));
-    //}
     protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
     {
+        SendTelemetry();
+
         await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
         await VS.Settings.OpenAsync<OptionsProvider.GeneralOptions>();
     }
 
+    private void SendTelemetry()
+    {
+        Task.Run(async () =>
+        {
+            var telemetryManager = await VS.GetMefServiceAsync<ITelemetryManager>();
+            telemetryManager.SendTelemetryAsync(Constants.Telemetry.OPEN_SETTINGS);
+        }).FireAndForget();
+    }
 }
