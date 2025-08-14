4d00730d353bbb17335e11cb3ae9b3dd85144b2e
Selma Copra <selma.copra@codescene.com>
2025-08-14 09:45:46 +0200

    Update required DevTools version

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/CliSettingsProvider.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/CliSettingsProvider.cs
@@ -8,7 +8,7 @@ namespace Codescene.VSExtension.Core.Application.Services.Cli
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class CliSettingsProvider : ICliSettingsProvider
     {
-        public string RequiredDevToolVersion => "835ba3afe7a0d6a1ed65912efa52a1b85ca059ea";
+        public string RequiredDevToolVersion => "99371f204d1502e328bf4fa26b4bdf3e6f05fd0f";
         public string CliArtifactName => $"cs-ide-windows-amd64-{RequiredDevToolVersion}.zip";
         public string CliArtifactUrl => $"{ArtifactBaseUrl}{CliArtifactName}";
         public string CliFileName => $"cs-ide.exe";
