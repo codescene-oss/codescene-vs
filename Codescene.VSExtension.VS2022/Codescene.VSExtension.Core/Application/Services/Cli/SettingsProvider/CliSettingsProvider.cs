9fa812b5232142f31ceddab9419969d5089541ae
Dzenan Granulo <dzenan.granulo@codescene.com>
2025-10-20 15:42:45 +0200

    revert manual version changes

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/SettingsProvider/CliSettingsProvider.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/SettingsProvider/CliSettingsProvider.cs
@@ -8,7 +8,7 @@ namespace Codescene.VSExtension.Core.Application.Services.Cli
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class CliSettingsProvider : ICliSettingsProvider
     {
-        public string RequiredDevToolVersion => "0d6512606c3dd5f26a2ed3368b160592e4faff72";
+        public string RequiredDevToolVersion => "2b500b540e0b17e20ff317aeae9486d83cddb6b4";
         public string CliArtifactName => $"cs-ide-windows-amd64-{RequiredDevToolVersion}.zip";
         public string CliArtifactUrl => $"{ArtifactBaseUrl}{CliArtifactName}";
         public string CliFileName => $"cs-ide.exe";
