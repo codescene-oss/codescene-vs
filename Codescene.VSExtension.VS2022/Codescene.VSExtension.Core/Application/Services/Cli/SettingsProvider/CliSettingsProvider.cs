7feda70be823d338b1f2812986010cd5a95ed323
Dzenan Granulo <dzenan.granulo@codescene.com>
2025-10-21 13:39:37 +0200

    feat: Bump CLI version to 1.0.14

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/SettingsProvider/CliSettingsProvider.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Services/Cli/SettingsProvider/CliSettingsProvider.cs
@@ -8,8 +8,8 @@ namespace Codescene.VSExtension.Core.Application.Services.Cli
     [PartCreationPolicy(CreationPolicy.Shared)]
     public class CliSettingsProvider : ICliSettingsProvider
     {
-        public string RequiredDevToolVersion => "2b500b540e0b17e20ff317aeae9486d83cddb6b4";
-        public string CliArtifactName => $"cs-ide-windows-amd64-{RequiredDevToolVersion}.zip";
+        public string RequiredDevToolVersion => "2b500b540e0b17e20ff317aeae9486d83cddb6b4"; // 1.0.14
+		public string CliArtifactName => $"cs-ide-windows-amd64-{RequiredDevToolVersion}.zip";
         public string CliArtifactUrl => $"{ArtifactBaseUrl}{CliArtifactName}";
         public string CliFileName => $"cs-ide.exe";
         public string ArtifactBaseUrl => "https://downloads.codescene.io/enterprise/cli/";
