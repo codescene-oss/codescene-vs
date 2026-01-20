1dedfb44ffb3dc37d7a8b7aee13421893474b5ad
Martin Säfsten <martin.safsten@codescene.com>
2026-01-20 16:30:10 +0100

    switch from lcov to opencover

---
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core.IntegrationTests/CliExecutor/BaseCliExecutorTests.cs
@@ -0,0 +1,38 @@
+﻿using Codescene.VSExtension.Core.Application.Services.Cli;
+
+namespace Codescene.VSExtension.Core.IntegrationTests.CliExecutor
+{
+    public abstract class BaseCliExecutorTests: BaseIntegrationTests
+    {
+        protected ICliExecutor CliExecutor;
+        protected string TempCacheDir;
+
+
+        public new virtual void Initialize()
+        {
+            base.Initialize();
+            CliExecutor = GetService<ICliExecutor>();
+
+            TempCacheDir = Path.Combine(Path.GetTempPath(), "codescene-test-cache", Guid.NewGuid().ToString());
+            Directory.CreateDirectory(TempCacheDir);
+
+            MockCacheStorageService.Setup(x => x.GetSolutionReviewCacheLocation())
+                .Returns(TempCacheDir);
+        }
+
+        public virtual void Cleanup()
+        {
+            if (Directory.Exists(TempCacheDir))
+            {
+                try
+                {
+                    Directory.Delete(TempCacheDir, recursive: true);
+                }
+                catch
+                {
+                    // Ignore cleanup errors
+                }
+            }
+        }
+    }
+}
