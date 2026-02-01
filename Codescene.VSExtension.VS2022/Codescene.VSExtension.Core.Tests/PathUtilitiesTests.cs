a186ed9641a822c7e51a0dcd80156e43c7fd208e
vemv <vemv@users.noreply.github.com>
2026-02-01 23:42:54 +0100

    Add PathUtilitiesTests

---
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core.Tests/PathUtilitiesTests.cs
@@ -0,0 +1,96 @@
+using Codescene.VSExtension.Core.Application.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Codescene.VSExtension.Core.Tests
+{
+    [TestClass]
+    public class PathUtilitiesTests
+    {
+        [TestMethod]
+        public void GetRelativePath_NullBasePath_ReturnsFullPath()
+        {
+            var fullPath = @"C:\test\file.txt";
+            var result = PathUtilities.GetRelativePath(null, fullPath);
+            Assert.AreEqual(fullPath, result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_EmptyBasePath_ReturnsFullPath()
+        {
+            var fullPath = @"C:\test\file.txt";
+            var result = PathUtilities.GetRelativePath("", fullPath);
+            Assert.AreEqual(fullPath, result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_NullFullPath_ReturnsNull()
+        {
+            var basePath = @"C:\test";
+            var result = PathUtilities.GetRelativePath(basePath, null);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_EmptyFullPath_ReturnsEmpty()
+        {
+            var basePath = @"C:\test";
+            var result = PathUtilities.GetRelativePath(basePath, "");
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_InvalidBasePath_ReturnsFullPath()
+        {
+            var invalidBasePath = "http://";
+            var fullPath = @"C:\test\file.txt";
+            var result = PathUtilities.GetRelativePath(invalidBasePath, fullPath);
+            Assert.AreEqual(fullPath, result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_InvalidFullPath_ReturnsFullPath()
+        {
+            var basePath = @"C:\test";
+            var invalidFullPath = "not-a-valid-path-with-<>|";
+            var result = PathUtilities.GetRelativePath(basePath, invalidFullPath);
+            Assert.AreEqual(invalidFullPath, result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_RelativeBasePath_CatchesException()
+        {
+            var relativeBasePath = "relative\\path";
+            var fullPath = @"C:\test\file.txt";
+            var result = PathUtilities.GetRelativePath(relativeBasePath, fullPath);
+            Assert.AreEqual(fullPath, result);
+        }
+
+        [TestMethod]
+        public void GetRelativePath_ValidPaths_ReturnsRelativePath()
+        {
+            var basePath = @"C:\test\project";
+            var fullPath = @"C:\test\project\src\file.txt";
+            var result = PathUtilities.GetRelativePath(basePath, fullPath);
+            Assert.IsTrue(result.Contains("src"));
+            Assert.IsTrue(result.Contains("file.txt"));
+        }
+
+        [TestMethod]
+        public void AppendDirectorySeparatorChar_PathAlreadyEndsWithSeparator_ReturnsUnchanged()
+        {
+            var pathWithSeparator = @"C:\test\";
+            var result = PathUtilities.AppendDirectorySeparatorChar(pathWithSeparator);
+            Assert.AreEqual(pathWithSeparator, result);
+        }
+
+        [TestMethod]
+        public void AppendDirectorySeparatorChar_PathWithoutSeparator_AppendsSeparator()
+        {
+            var pathWithoutSeparator = @"C:\test";
+            var result = PathUtilities.AppendDirectorySeparatorChar(pathWithoutSeparator);
+            Assert.IsTrue(result.EndsWith(Path.DirectorySeparatorChar.ToString()));
+            Assert.AreEqual(pathWithoutSeparator + Path.DirectorySeparatorChar, result);
+        }
+    }
+}
