08b3fb5c34a5291a5b01a1e86303365dbf63c781
vemvcs <victor.valenzuela+gh@codescene.com>
2026-02-01 21:44:34 +0100

    Add FileChangeEvent (#117)
    Co-authored-by: vemv <vemv@users.noreply.github.com>

---
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Application/Git/FileChangeEvent.cs
@@ -0,0 +1,16 @@
+using Codescene.VSExtension.Core.Enums.Git;
+
+namespace Codescene.VSExtension.Core.Application.Git
+{
+    public class FileChangeEvent
+    {
+        public FileChangeType Type { get; }
+        public string FilePath { get; }
+
+        public FileChangeEvent(FileChangeType type, string filePath)
+        {
+            Type = type;
+            FilePath = filePath;
+        }
+    }
+}
