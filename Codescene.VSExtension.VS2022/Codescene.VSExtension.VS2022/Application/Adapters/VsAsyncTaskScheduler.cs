e6684b7ac4ab7653cf372317e756152cd497066c
vemvcs <victor.valenzuela+gh@codescene.com>
2026-02-03 07:45:28 +0100

    Introduce GitChangeObserver (#121)
    Co-authored-by: vemv <vemv@users.noreply.github.com>

---
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.VS2022/Application/Adapters/VsAsyncTaskScheduler.cs
@@ -0,0 +1,32 @@
+using Codescene.VSExtension.Core.Interfaces;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Codescene.VSExtension.VS2022.Application.Adapters
+{
+    [Export(typeof(IAsyncTaskScheduler))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public class VsAsyncTaskScheduler : IAsyncTaskScheduler
+    {
+        public void Schedule(Func<Task> asyncWork)
+        {
+            try
+            {
+                ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                {
+                    await asyncWork();
+                }).FileAndForget("VsAsyncTaskScheduler/ScheduledWork");
+            }
+            catch
+            {
+                Task.Run(async () =>
+                {
+                    try { await asyncWork(); }
+                    catch (Exception) { }
+                });
+            }
+        }
+    }
+}
