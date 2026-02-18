aa31e54c2556f38df7bfd29977ffd40c49b3012e
vemv <vemv@users.noreply.github.com>
2026-02-18 01:54:35 +0100

    Bump project version

--- Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Properties/AssemblyInfo.cs
+++ Codescene.VSExtension.VS2022/Codescene.VSExtension.Core/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.5.0")]
+[assembly: AssemblyFileVersion("0.4.5.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Codescene.VSExtension.Core.Tests")]
